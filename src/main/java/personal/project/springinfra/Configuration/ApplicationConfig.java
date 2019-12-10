d1cfdf864ca96279f6f967fdecfa7eb815b7d755
arVahedi <ar.vahedi357@gmail.com>
2019-12-11 00:55:27 +0330

    Sync commit
    Signed-off-by: arVahedi <ar.vahedi357@gmail.com>

--- src/main/java/personal/project/springinfra/Configuration/ApplicationConfig.java
+++ src/main/java/personal/project/springinfra/Configuration/ApplicationConfig.java
@@ -3,5 +3,5 @@ package personal.project.springinfra.Configuration;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class ApplicationConfig {
+public class ApplicationConfig extends BaseConfig {
 }
