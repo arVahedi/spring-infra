6dc983b62fc5ffc41a5f89f0b208dbb0d700b317
arVahedi <ar.vahedi357@gmail.com>
2021-06-01 18:04:57 +0430

    Fixed lombok static import in Constant class
    Signed-off-by: arVahedi <ar.vahedi357@gmail.com>

---
+++ src/main/java/personal/project/springinfra/assets/Constant.java
@@ -0,0 +1,10 @@
+package personal.project.springinfra.assets;
+
+public class Constant {
+
+    private Constant() {
+        throw new java.lang.UnsupportedOperationException("This is a utility class and cannot be instantiated");
+    }
+
+    public static final String PASSWORD_MASK = "******";
+}
--- src/main/java/personal/project/springinfra/assets/Constants.java
+++
@@ -1,8 +0,0 @@
-package personal.project.springinfra.assets;
-
-import lombok.experimental.UtilityClass;
-
-@UtilityClass
-public class Constants {
-    public final String PASSWORD_PLACEHOLDER = "******";
-}
