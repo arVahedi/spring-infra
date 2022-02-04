37711fea2706d626fa80bfda5fadc59013225176
arVahedi <ar.vahedi357@gmail.com>
2022-02-05 02:56:58 +0330

    Setup auto redirection to TLS for http protocol
    Signed-off-by: arVahedi <ar.vahedi357@gmail.com>

--- src/main/java/personal/project/springinfra/configuration/BeanConfig.java
+++ src/main/java/personal/project/springinfra/configuration/BeanConfig.java
@@ -3,13 +3,6 @@ package personal.project.springinfra.configuration;
 import io.micrometer.core.aop.TimedAspect;
 import io.micrometer.core.instrument.MeterRegistry;
 import lombok.RequiredArgsConstructor;
-import org.apache.catalina.Context;
-import org.apache.catalina.connector.Connector;
-import org.apache.tomcat.util.descriptor.web.SecurityCollection;
-import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
-import org.springframework.boot.autoconfigure.web.ServerProperties;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,37 +10,9 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class BeanConfig implements BaseConfig {
 
-    private final ServerProperties serverProperties;
-
     //TimedAspect makes @Timed usable on any arbitrary method.
     @Bean
     public TimedAspect timedAspect(MeterRegistry registry) {
         return new TimedAspect(registry);
     }
-
-    /*@Bean
-    public ServletWebServerFactory servletContainer() {
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
-            @Override
-            protected void postProcessContext(Context context) {
-                SecurityConstraint securityConstraint = new SecurityConstraint();
-                securityConstraint.setUserConstraint("CONFIDENTIAL");
-                SecurityCollection collection = new SecurityCollection();
-                collection.addPattern("/*");
-                securityConstraint.addCollection(collection);
-                context.addConstraint(securityConstraint);
-            }
-        };
-        tomcat.addAdditionalTomcatConnectors(redirectConnector());
-        return tomcat;
-    }
-
-    private Connector redirectConnector() {
-        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-        connector.setScheme("http");
-        connector.setPort(8080);
-        connector.setSecure(false);
-        connector.setRedirectPort(this.serverProperties.getPort());
-        return connector;
-    }*/
 }
